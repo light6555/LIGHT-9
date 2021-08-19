4772f3a45feb95e280dfbe61a7672964d86ec4ff
light6555 <89099527+light6555@users.noreply.github.com>
2021-08-19 09:51:39 -0300

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,18 @@
+const version = (prefix, pushname) => {
+    return `
+*LIGHT-BOT-2 🤖*
+
+*Actualizado:* 19 de agosto
+*Versión actual:* 2
+*Ofrecida por:* LIGHT
+
+Para conocer la ultima versión de LIGHT-BOT-2 entra al siguiente blog
+
+
+
+by LIGHT
+`
+
+}
+
+exports.version = version
